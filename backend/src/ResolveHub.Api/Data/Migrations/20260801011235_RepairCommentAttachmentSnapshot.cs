feff4f99053d432b58731f6db93c64e79665444c
Fatima Ghannam <fatimaghannam268@gmail.com>
2026-08-01 04:23:07 +0300

    Repair EF Core migration snapshot for comment attachments

---
+++ backend/src/ResolveHub.Api/Data/Migrations/20260801011235_RepairCommentAttachmentSnapshot.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ResolveHub.Api.Data.Migrations
+{
+    public partial class RepairCommentAttachmentSnapshot : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
